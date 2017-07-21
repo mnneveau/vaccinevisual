058342d1e36bde72b691c1a12a8960694d105712
Megan Neveau <mneveau@linus86.gsc.wustl.edu>
2017-07-21 15:35:47 -0500

    png addition

--- createVaccineVisualization.py
+++ createVaccineVisualization.py
@@ -2,15 +2,25 @@
 import sys, os
 import turtle
 
-def output_screen():
-    ps_file = "vaccine.ps"
-    pdf_file = "vaccine.pdf"
-    ts = t.getscreen()
-    ts.getcanvas().postscript(file=ps_file)
-    os.system('convert -density 100 -quality 200 ' + ps_file + " " + pdf_file)
+os.environ["DISPLAY"] = ':0.0'
+
+#determine what proportion of circle each peptide should take up
+def get_conversion_factor(pep_seqs):
+    total_len = 0
+    for pep in pep_seqs:
+        total_len += len(pep)   
+    #30 degrees reserved for white space
+    conversion_factor = 330 / total_len
+    return(conversion_factor)
+
+def draw_header(t, header_pos):
+    t.pu()
+    t.setpos(header_pos)
+    t.write("Vaccine Design", align="center", font=("Arial", 18, "bold"))
+    t.pd()
     return()
 
-def draw_wht_space():
+def draw_wht_space(t, circle_radius, wht_space_angle):
     t.pencolor("white")
     t.circle(circle_radius,wht_space_angle)
     return()
@@ -19,14 +29,13 @@ def draw_wht_space():
 def get_color(count):
     #option 1: 3 blue/green color scheme
     #scheme = [(161,218,180),(65,182,196),(44,127,184),(37,52,148)]
-    #option 2: 11 color scheme
-    #scheme = [(166,206,227),(31,120,180),(178,223,138),(51,160,44),(251,154,153),(227,26,28),(253,191,111),(255,127,0),(202,178,214),(106,61,154),(177,89,40)]
+    #option 2: 6 color scheme
     scheme = [(31,120,180),(51,160,44),(227,26,28),(255,127,0),(106,61,154),(177,89,40)]
     count =  count % len(scheme)
     return scheme[count]
 
 #draw perpindicular line to arc to mark junction
-def draw_junction_w_label(junct_score):
+def draw_junction_w_label(junct_score, t, pen_thin, junct_score_space, angle):
     reset = t.heading()
     t.rt(90)
     t.pencolor("black")
@@ -34,16 +43,44 @@ def draw_junction_w_label(junct_score):
     t.forward(10)
     t.back(20)
     t.pu()
-    t.back(junct_score_space)
-    t.write(junct_score, align="center")
-    t.forward(junct_score_space)
+    #t.back(junct_score_space)
+    if (angle >= 0 and angle < 70):
+        t.back(15)
+        t.write(junct_score + 'nM', align="center")
+        t.forward(15)
+    elif (angle >= 65 and angle < 115):
+        t.back(10)
+        t.write(junct_score + 'nM', align="center")
+        t.forward(10)
+    elif (angle >= 115 and angle < 165):
+        t.back(20)
+        t.write(junct_score + 'nM', align="center")
+        t.forward(20)
+    elif (angle >= 165 and angle < 195):
+        t.back(25)
+        t.write(junct_score + 'nM', align="center")
+        t.forward(25)
+    elif (angle >= 195 and angle < 245):
+        t.back(35)
+        t.write(junct_score + 'nM', align="center")
+        t.forward(35)
+    elif (angle >= 245 and angle < 295):
+        t.back(15)
+        t.write(junct_score + 'nM', align="center")
+        t.forward(15)
+    else:
+        t.back(20)
+        t.write(junct_score + 'nM', align="center")
+        t.forward(20)
+    #t.write(junct_score + "nM", align="center")
+    #t.forward(junct_score_space)
     t.pd()
     t.forward(10)
     t.setheading(reset)
     return()
 
 #draw second line of junctions with amino acid additions
-def draw_junction():
+def draw_junction(t, pen_thin):
     reset = t.heading()
     t.rt(90)
     t.pencolor("black")
@@ -55,7 +92,7 @@ def draw_junction():
     return()
 
 #draw arc for peptide
-def draw_arc_peptide(peptide, length, count, angle):
+def draw_arc_peptide(peptide, length, count, angle, t, circle_radius, conversion_factor, pep_id_space):
     t.pencolor(get_color(count))
     t.circle(circle_radius, (conversion_factor * length) / 2)
     t.pu()
@@ -74,7 +111,7 @@ def draw_arc_peptide(peptide, length, count, angle):
     t.circle(circle_radius, (conversion_factor * length) / 2)
 
 #draw arc for amino acid inserts to junctions
-def draw_arc_junct(peptide, length):
+def draw_arc_junct(peptide, length, t, conversion_factor, junct_seq_space, circle_radius):
     #t.pencolor("black")
     t.circle(circle_radius, (conversion_factor * length) / 2)
     t.pu()
@@ -87,104 +124,111 @@ def draw_arc_junct(peptide, length):
     t.pd()
     t.circle(circle_radius, (conversion_factor * length) / 2)
 
+#print turtle screen to a postscript file, convert to pdf
+def output_screen(t):
+    ps_file = "vaccine.ps"
+    pdf_file = "vaccine.pdf"
+    ts = t.getscreen()
+    ts.getcanvas().postscript(file=ps_file)
+    os.system('convert -density 100 -quality 200 ' + ps_file + " " + pdf_file)
+    os.system('rm ' + ps_file)
+    return()
 
-[script, input_file] = sys.argv
+def main(input_file=sys.argv[1]):
+    min_pep_length = 8
+    max_pep_length = 100
 
-min_pep_length = 8
-max_pep_length = 100
-
-pep_seqs = []
-with open(input_file, 'r') as input_f:
-    header = input_f.readline().strip()
-    for line in input_f:
-        pep_seqs.append(line.strip())
+    pep_seqs = []
+    with open(input_file, 'r') as input_f:
+        header = input_f.readline().strip()
+        for line in input_f:
+            pep_seqs.append(line.strip())
 #remove >, get peptide names and junction scores from FASTA input
-edited_header = header[1:]
-fields = edited_header.split("|")
-pep_ids_joined = fields[0].split(",")
-pep_ids = []
-for pep_id in pep_ids_joined:
-    if "." in pep_id:
-        mt, gene, var = pep_id.split(".")
-        pep_id = "-".join((gene,var))
-    pep_ids.append(pep_id)
-junct_scores = fields[3].split(":")
-junct_scores = junct_scores[1].split(",")
+    edited_header = header[1:]
+    fields = edited_header.split("|")
+    pep_ids_joined = fields[0].split(",")
+    pep_ids = []
+    for pep_id in pep_ids_joined:
+        if "." in pep_id:
+            mt, gene, var = pep_id.split(".")
+            pep_id = "-".join((gene,var))
+        pep_ids.append(pep_id)
+    junct_scores = fields[3].split(":")
+    junct_scores = junct_scores[1].split(",")
 
 #Error if not a peptide sequence for every peptide ID
-if len(pep_ids) != len(pep_seqs):
-    print("Error: Not an equal number of peptide sequences and peptide IDs")
-    sys.exit()
+    if len(pep_ids) != len(pep_seqs):
+        print("Error: Not an equal number of peptide sequences and peptide IDs")
+        sys.exit()
 
-#determine what proportion of circle each peptide should take up
-total_len = 0
-for pep in pep_seqs:
-    total_len += len(pep)   
-    #30 degrees reserved for white space
-    conversion_factor = 330 / total_len
+    conversion_factor = get_conversion_factor(pep_seqs)
+
+    #determine number of peptides (not including junction additions)
+    num_peptides = 0
+    for pep in pep_seqs:
+        length = len(pep)
+        if length > 8 and length < 25:
+            num_peptides += 1
 
 #draw vaccine
-t = turtle.Turtle()
-myWin = turtle.Screen()
-turtle.colormode(255)
-turtle.mode("logo")
-t.speed(0)
-t.hideturtle()
+    t = turtle.Turtle()
+    myWin = turtle.Screen()
+    turtle.colormode(255)
+    turtle.mode("logo")
+    t.speed(0)
+    t.hideturtle()
 
-circle_radius = -200
-circle_pos = (-200,0)
-header_pos = (0,0)
-wht_space_angle = 15
-pen_thick = 5
-pen_thin = 2
-pep_id_space = 65
-junct_seq_space = 25
-junct_score_space = 22
-
-#write header
-t.pu()
-t.setpos(header_pos)
-t.write("Vaccine Design", align="center", font=("Arial", 18, "bold"))
-t.pd()
-t.pu()
-t.setpos(circle_pos)
-t.pd()
-t.pensize(pen_thick)
-angle_parsed = 0
-#add white space in circle before genes
 #negative radius draws circle clockwise
-draw_wht_space()
-angle_parsed += wht_space_angle
-draw_junction()
+    circle_radius = -200
+    circle_pos = (-200,0)
+    header_pos = (0,0)
+    wht_space_angle = 15
+    pen_thick = 5
+    pen_thin = 2
+    pep_id_space = 45 + num_peptides 
+    junct_seq_space = 25
+    junct_score_space = 40
+
+    draw_header(t, header_pos)
+    t.pu()
+    t.setpos(circle_pos)
+    t.pd()
+    t.pensize(pen_thick)
+    angle_parsed = 0
+#add white space in circle before genes
+    draw_wht_space(t, circle_radius, wht_space_angle)
+    angle_parsed += wht_space_angle
+    draw_junction(t, pen_thin)
 
 #draw main part of circle
-junctions_parsed = 0
-peptides_parsed = 0
-for pep in pep_seqs:
-    pep_length = len(pep)
-    peptide = pep_ids[peptides_parsed]
-    t.pensize(pen_thick)
-    angle_parsed += conversion_factor * pep_length
+    junctions_parsed = 0
+    peptides_parsed = 0
+    for pep in pep_seqs:
+        pep_length = len(pep)
+        peptide = pep_ids[peptides_parsed]
+        t.pensize(pen_thick)
+        angle_parsed += conversion_factor * pep_length
     #if length within reasonable range, draw and label arc for peptide
-    if pep_length > min_pep_length and pep_length < max_pep_length:
-        draw_arc_peptide(peptide, pep_length, junctions_parsed, angle_parsed)
-        if junctions_parsed < len(junct_scores):
-            draw_junction_w_label(junct_scores[junctions_parsed])
-            junctions_parsed += 1
+        if pep_length > min_pep_length and pep_length < max_pep_length:
+            draw_arc_peptide(peptide, pep_length, junctions_parsed, angle_parsed, t, circle_radius, conversion_factor, pep_id_space)
+            if junctions_parsed < len(junct_scores):
+                draw_junction_w_label(junct_scores[junctions_parsed], t, pen_thin, junct_score_space, angle_parsed)
+                junctions_parsed += 1
     #if length is less than minimum peptide length, assume amino acid addition to junction
-    elif pep_length < min_pep_length:
-        draw_arc_junct(peptide, pep_length)
-        draw_junction()
-    else:
-        print("Error: Peptide sequence over 100 amino acids inputted")
-        sys.exit()
-    peptides_parsed += 1
+        elif pep_length < min_pep_length:
+                draw_arc_junct(peptide, pep_length, t, conversion_factor, junct_seq_space, circle_radius)
+                draw_junction(t, pen_thin)
+        else:
+                print("Error: Peptide sequence over 100 amino acids inputted")
+                sys.exit()
+        peptides_parsed += 1
 
 #add white space in circle after genes    
-draw_junction()
-draw_wht_space()
-output_screen()
+    draw_junction(t, pen_thin)
+    draw_wht_space(t, circle_radius, wht_space_angle)
+    output_screen(t)
 
-turtle.mainloop()
+    turtle.mainloop()
 
+main()
 
